f3760adabad378da75d25bdbb803c39746c37945
estherrwu <eswu.32@gmail.com>
2024-01-16 17:38:54 -0800

    toggle menu

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Change images in website
 
+function toggleMenu() {
+    var menu = document.getElementById("menuItems");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+
 const imageURLs = ["https://www.vinylmeplease.com/cdn/shop/products/everything_i_know_about_love_store.jpg?q=60&auto=format&w=926",
     "https://squarecatvinyl.com/cdn/shop/files/Screenshot_2023-08-11_at_11.17.55_AM_022b6110_thumbnail_1024.webp?v=1696525110"
 ];
